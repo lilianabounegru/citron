af2bbcb20fde302bf20180f324e422e3f1fcadb3
Liliana Bounegru <bounegru@ejc.net>
2018-04-15 12:08:54 +0100

    update
    update

--- citron.py
+++ citron.py
@@ -2,13 +2,14 @@ import requests
 import json
 
 res = "filepath"                                                                                                                                                                                                                                                      
-fhand = open("links.txt")
-for line in fhand:
-	response = requests.post(
-    	"http://citron.virt.ch.bbc.co.uk/quotes",
-    	headers={"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
-    	data={"text": fhand}
-	)
+with open("links.txt") as f:
+	listy = f.readlines()
+	for line in listy:
+		response = requests.post(
+    		"http://citron.virt.ch.bbc.co.uk/quotes",
+    		headers={"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+    		data={"text": line}
+			)
 	if response is not None:
-		with open(res, "w", encoding="utf-8") as outfile:
+		with open(res, "a", encoding="utf-8") as outfile:
 			json.dump(response.json(), outfile, ensure_ascii=False, indent=4)
